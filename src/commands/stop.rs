719716c6a3e6a71a8a9eec29ba61c6dd3a31cb83
chancedrigor <chancedrigor@gmail.com>
2022-11-21 09:15:27 -0500

    Reply and leave concurrently

--- src/commands/stop.rs
+++ src/commands/stop.rs
@@ -1,10 +1,11 @@
 use log::instrument;
+use poise::futures_util::TryFutureExt;
 
-use crate::{log, Context, Result};
+use crate::{log, Context, Error, Result};
 
 /// Stop the bot, delete the queue, and leave the call.
 #[instrument]
-#[poise::command(slash_command, guild_only, guild_cooldown = 2)]
+#[poise::command(slash_command, guild_only)]
 pub async fn stop(ctx: Context<'_>) -> Result<()> {
     let manager = songbird::get(ctx.discord())
         .await
@@ -18,9 +19,19 @@ pub async fn stop(ctx: Context<'_>) -> Result<()> {
         .clone();
 
     let mut call_lock = call.lock().await;
+
+    // Check if in a voice channel
+    if call_lock.current_channel().is_none() {
+        return Err(log::eyre!("I'm not in a voice channel."));
+    }
+
+    let leave = call_lock.leave().map_err(|e| -> Error { e.into() });
+    let reply = ctx.say("Buh bye!").map_err(|e| -> Error { e.into() });
+    tokio::try_join!(leave, reply)?;
+
+    // Stop everything
     let queue = call_lock.queue();
     queue.stop();
-    call_lock.leave().await?;
-    ctx.say("Buh bye!").await?;
+
     Ok(())
 }
