e36af324984ab552309ddce26b7e69548f7dc68c
chancedrigor <chancedrigor@gmail.com>
2022-11-21 09:18:56 -0500

    Added event handlers for stopping the bot on dc and leaving empty rooms after 5 mins

--- src/bot/voice.rs
+++ src/bot/voice.rs
@@ -1,15 +1,16 @@
 use std::sync::Arc;
 
 use log::eyre;
+use poise::{async_trait, serenity_prelude as serenity};
+use songbird::{Call, Event, EventContext, EventHandler};
 use tokio::sync::Mutex;
-// use poise::serenity_prelude as serenity;
 use tracing::instrument;
 
 use crate::{log, Context, Result};
 
 /// Joins the author's voice channel based on the given context.
 #[instrument(skip(ctx), fields(author=%ctx.author(), guild=?ctx.guild_id()))]
-pub async fn join_author(ctx: &Context<'_>) -> Result<Arc<Mutex<songbird::Call>>> {
+pub async fn join_author(ctx: &Context<'_>) -> Result<Arc<Mutex<Call>>> {
     let manager = songbird::get(ctx.discord())
         .await
         .expect("expected songbird manager");
@@ -44,5 +45,111 @@ pub async fn join_author(ctx: &Context<'_>) -> Result<Arc<Mutex<songbird::Call>>
 
     let (call, join_result) = manager.join(guild_id, channel_id).await;
     join_result?;
-    Ok(call)
+
+    let empty_leaver = EmptyChannelLeaver {
+        call: call.clone(),
+        channel_id,
+        ctx: ctx.discord().to_owned(),
+    };
+
+    let dc_stop = StopOnDisconnect { call: call.clone() };
+    let mut call_lock = call.lock().await;
+    call_lock.add_global_event(
+        Event::Periodic(std::time::Duration::from_secs(5 * 60), None),
+        empty_leaver,
+    );
+    call_lock.add_global_event(Event::Core(songbird::CoreEvent::DriverConnect), dc_stop);
+
+    Ok(call.clone())
+}
+
+/// Unwrap the given option, logging an unexpected `None` with the given message.
+/// Cancels the event listener instead of panic.
+macro_rules! unwrap_option_cancel {
+    ($opt:expr, $msg:tt) => {
+        match $opt {
+            Some(i) => i,
+            None => {
+                log::error!($msg);
+                return Some(Event::Cancel);
+            }
+        }
+    };
+}
+
+/// Unwrap the given result, logging an unexpected `Err` with the given message.
+/// The given `id` can be used in the fstring message to displat the error
+/// Cancels the event listener instead of panic.
+///
+/// # Examples
+/// ```
+/// fn test() -> Option<Event> {
+///     let res: Result<i32, &str> = Err("math hard");
+///     let num: i32 = unwrap_result_cancel!(res, e, "Failed with error: {e:#}");
+///     None
+/// }
+/// ```
+///
+macro_rules! unwrap_result_cancel {
+    ($opt:expr, $id:ident, $msg:tt) => {
+        match $opt {
+            Ok(i) => i,
+            Err($id) => {
+                log::error!($msg);
+                return Some(Event::Cancel);
+            }
+        }
+    };
+}
+
+struct EmptyChannelLeaver {
+    ctx: serenity::Context,
+    channel_id: serenity::ChannelId,
+    call: Arc<Mutex<Call>>,
+}
+
+#[async_trait]
+impl EventHandler for EmptyChannelLeaver {
+    async fn act(&self, _ectx: &EventContext<'_>) -> Option<Event> {
+        let channel = unwrap_result_cancel!(
+            self.channel_id.to_channel(&self.ctx).await,
+            e,
+            "Could not find channel: {e:#?}"
+        );
+
+        let guild_channel = unwrap_option_cancel!(channel.guild(), "Expected guild channel");
+
+        let members = unwrap_result_cancel!(
+            guild_channel.members(&self.ctx).await, // Should never panic
+            e,
+            "Could not find channel members: {e:#?}"
+        );
+
+        // Whether the channel has non-bot members
+        let has_members = members.iter().any(|m| !m.user.bot);
+
+        if has_members {
+            None // Do nothing but keep the event listener.
+        } else {
+            unwrap_result_cancel!(
+                self.call.lock().await.leave().await, // Leave the channel
+                e,
+                "Error leaving channel: {e:#?}"
+            );
+            Some(Event::Cancel) // Cancel the event listener
+        }
+    }
+}
+
+struct StopOnDisconnect {
+    call: Arc<Mutex<Call>>,
+}
+
+#[async_trait]
+impl EventHandler for StopOnDisconnect {
+    async fn act(&self, _ectx: &EventContext<'_>) -> Option<Event> {
+        let call_lock = self.call.lock().await;
+        call_lock.queue().stop();
+        Some(Event::Cancel)
+    }
 }
